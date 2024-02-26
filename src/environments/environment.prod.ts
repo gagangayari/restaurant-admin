dbe5d04e3035ba5effa275c728422f1e39182ffa
gagangayari <gagangayari@gmail.com>
2024-02-27 00:56:16 +0530

    Modified production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
+  base : "https://firestore.googleapis.com/v1/",
+  // projectUrl : "/projects/YOUR_PROJECT_ID/databases/(default)/documents/cities/LA",
+  apiKey: "AIzaSyCYvEDSR4VbbjGUGzXXAKzFBG0rRsYKd5c",
   production: true
 };
